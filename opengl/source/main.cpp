1226ba741079af154311e2015fa649e33aa58dab
ilyata76 <ilyata76@yandex.ru>
2022-05-01 22:11:46 +0300

    has beed added freeglut (но всё равно с dll проблемы. как её решать?)

--- opengl/source/main.cpp
+++ opengl/source/main.cpp
@@ -3,13 +3,15 @@
 #include "include/sample/point-spin.hpp"
 #include "include/sample/creator_of_vertex_arr.hpp"
 
+#include "libraries/freeglut/include/GL/freeglut.h"
+
 #include <iostream>
 #include <ctime>
 
 // TODO: слой первый или второй  а материал?
 // TODO: нет проверок соответствует ли количество строк тому, что написано в начале файла
 
-int main(int argc, char* argv) {
+int main(int argc, char** argv) {
 	try {
 		//time_t aaa;
 		//std::wcout << (aaa = std::time(0));
@@ -19,11 +21,11 @@ int main(int argc, char* argv) {
 		//// какой-нибудь понятный интерфейс флажков типа FILES:1,100 - от 1 до 100 файлы проверять будет
 		//// visvamp convert <ывфадфывдплфвы>
 		//// visvamp visualize <dasfkasdfl>
-		std::wstring a = L"../temp";
-		////std::cout << vampire5::parser::get_count("");
-		std::vector<vvis::creator::Vertex> A = vvis::creator::create_arry(a)(9);
-		using namespace vvis::creator;
-		std::wcout << A[2].get_point().get('x');
+		//std::wstring a = L"../temp";
+		//////std::cout << vampire5::parser::get_count("");
+		//std::vector<vvis::creator::Vertex> A = vvis::creator::create_arry(a)(9);
+		//using namespace vvis::creator;
+		//std::wcout << A[2].get_point().get('x');
 
 		//auto abobus = vampire5::parser::get_string(a);
 		//std::wstring aa = abobus(9);
@@ -39,7 +41,6 @@ int main(int argc, char* argv) {
 		//vvis::creator::Spin ss(aboba, 2.0, 3.0);
 		//std::wcout << ss.get(L'x');
 		//std::wcout << ss.get(L"sx");
-
 	}
 	catch (Exceptio& E) {
 		_wsetlocale(LC_ALL, L".UTF-8"); // !!! ГДЕ ВЫ БЫЛИ РАНЬШЕ С ВАШИМ .UTF-8
