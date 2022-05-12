30efbdca53b893baa29f72e801f733cbee9d737b
ilyata76 <ilyata76@yandex.ru>
2022-05-13 00:57:25 +0300

    cleaned cpp file

--- opengl/source/include/sample/drawing_shape_opengl.hpp
+++ opengl/source/include/sample/drawing_shape_opengl.hpp
@@ -70,25 +70,6 @@
 			void n_keys(unsigned char key, int x, int y);
 			void s_keys(int key, int x, int y);
 
-			
-			/// <summary>
-			/// ///////////
-			/// </summary>
-			
-			void display_nothing();
-			void display_l();
-
-			void draw_cone_spin(int& index, bool color);
-			void display_cone();
-			
-			void normal_keys(unsigned char key, int x, int y);
-			void special_keys(int key, int x, int y);
-			
-			// if index = DRAW_ALL (-1) draw all of vector
-			void main_glut(int argc, char** argv, std::vector <vvis::creator::Vertex>& vect, wchar_t shape, bool color, int index);
-
-			void myReshape(int w, int h);
-
 		}
 	}
 
