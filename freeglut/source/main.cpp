246f26ea258d3f16e79ada7446e22733c92b4324
ilyata76 <ilyata76@yandex.ru>
2022-06-07 23:17:58 +0300

    settings.get

--- freeglut/source/main.cpp
+++ freeglut/source/main.cpp
@@ -6,10 +6,9 @@
 int main(int argc, char** argv) {
 	Global_settings a;
 	Freeglut_settings b;
-	a.number_of_file = 100;
 	Settings AA (a, b);
 	AA.global_settings.path_to_folder = L".";
-	AA.save(L'a');
+	AA.get(L'a');
 	AA.print_only_freeglut(std::wcout);
 	AA.print_only_global(std::wcout);
 	system("pause");
