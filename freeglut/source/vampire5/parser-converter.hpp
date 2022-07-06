b1024e2810228dd8989247a629d7715c18a6090f
ilyata76 <ilyata76@yandex.ru>
2022-07-06 12:40:19 +0300

    del tabs

--- freeglut/source/vampire5/parser-converter.hpp
+++ freeglut/source/vampire5/parser-converter.hpp
@@ -4,9 +4,9 @@
 #ifndef PARSER_CONVERTER_VAMPIRE5_HPP
 #define PARSER_CONVERTER_VAMPIRE5_HPP
 
-		#include "../helpful/useful_functions.hpp"
-		#include <fstream>
-		#include "../libraries/exceptio/include/assert.hpp"
+	#include "../helpful/useful_functions.hpp"
+	#include <fstream>
+	#include "../libraries/exceptio/include/assert.hpp"
 
 	bool v5_parser_converter(const std::wstring& _path_to_folder, int _number_of_file);
 
