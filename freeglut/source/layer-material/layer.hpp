cc831db144297203529b4fce1687cfdb9f5df955
ilyata76 <ilyata76@yandex.ru>
2022-07-10 11:31:24 +0300

    layer hpp file

--- freeglut/source/layer-material/layer.hpp
+++ freeglut/source/layer-material/layer.hpp
@@ -1,1 +1,24 @@
-﻿
+﻿#pragma once
+
+#ifndef LAYER_HPP
+#define LAYER_HPP
+
+	#include "../color/RGB.hpp"
+
+	struct Layer {
+
+		Rgb background;
+		std::wstring name;
+		int number;
+
+		Layer() : background(Rgb(0.5, 0.5, 0.5)), name(L"undefined"), number(0) {};
+		Layer(Rgb& _background, std::wstring& _name, int _number) : background(_background), name(_name), number(_number) {};
+
+		friend std::wostream& operator<<(std::wostream& _out, const Layer& _settings);
+
+	};
+
+	Layer find_layer_in_vector(std::vector<Layer> _vct);
+
+
+#endif // !LAYER_HPP
